9bf1d5ba4a21a49216817741d96502db259636b3
Sean Ericson <sericson0@gmail.com>
2019-01-08 09:57:01 -0700

    update with more examples

--- Examples at end of book/SPE_Energy_Products.jl
+++ Examples at end of book/SPE_Energy_Products.jl
@@ -1,3 +1,4 @@
+#Example 4.4.1.1
 using JuMP, Clp, Complementarity
 
 producers = 1:4
@@ -10,15 +11,41 @@ costs = [10 20 30 5 6;
 		 17 40 5  3 7;
 		 50 23 28 12 4]
 
-m = Model(solver = ClpSolver())
 
-@variable(m, F[i in producers, j in consumers] >= 0)
-println(1)
-@objective(m, Min, sum(costs[i,j]*F[i,j] for i in producers, j in consumers))
-@constraint(m, supplyConst[i in producers], Supply[i] - sum(F[i,j] for j in consumers) >= 0)
-println(2)
-@constraint(m, demandConst[j in consumers], sum(F[i,j] for i in producers) - Demand[j] >= 0)
+#Using LP
+function LP_Model()
+	m = Model(solver = ClpSolver())
+	@variable(m, F[i in producers, j in consumers] >= 0)
+	@objective(m, Min, sum(costs[i,j]*F[i,j] for i in producers, j in consumers))
+	@constraint(m, supplyConst[i in producers], Supply[i] - sum(F[i,j] for j in consumers) >= 0)
+	@constraint(m, demandConst[j in consumers], sum(F[i,j] for i in producers) - Demand[j] >= 0)
 
-print(m)
-status = solve(m)
-println(getvalue(F))
+	# print(m)
+	status = solve(m)
+	println(getvalue(F))
+end
+
+
+#complementarity problem
+#Not working
+# function MCP_Model()
+# 	rho_s = [0.2 for i in producers]
+# 	kappa_s = [0 0 -7 0]
+# 	rho_d = [1 for j in consumers]
+# 	kappa_d = [29 46 15 13 42]
+
+# 	m = MCPModel()
+# 	@variable(m, x[i in producers, j in consumers] >= 0, start = 100)
+# 	@mapping(m, pairs[i in producers, j in consumers], rho_s[i]*sum(x[i, j1] for j1 in consumers) + kappa_s[i] + costs[i,j] + 
+# 		rho_d[j]*sum(x[i1, j] for i1 in producers) - kappa_d[j] >= 0)
+# 	@complementarity(m, pairs, x)
+	
+# 	print(m)
+# 	solveMCP(m, linear = true)
+
+# 	@show getvalue(x)
+# end
+
+
+LP_Model()
+# MCP_Model()
--- Examples at end of book/Stackelberg_MPEC_Firm_2_lead.jl
+++ Examples at end of book/Stackelberg_MPEC_Firm_2_lead.jl
@@ -1,7 +1,48 @@
 #Code for the Stackelberg MPEC with Firm 2 as leader
 using JuMP, Ipopt, Complementarity
 
+followers = 1:2
+production_process = 1:2
+C_l = [0.62 1.25]
+C_f = [0.55 0.81;
+	   0.78 1.35]
+K_l = [17000 22000]
+K_f = [21000 16000;
+       18000 14000]
+
+alpha = 2.5
+beta = 0.0000166666667
 
 m = Model(solver=IpoptSolver())
 
-@variable(x2())
+@variable(m, x_l[b in production_process] >= 0)
+@variable(m, x_f[i in followers, b in production_process] >= 0)
+@variable(m, q >= 0)
+@variable(m, gamma[i in followers, b in production_process] >= 0)
+##
+@objective(m, Min, sum(C_l[b]*x_l[b] - (alpha - beta*q)*x_l[b] for b in production_process))
+@constraint(m, q - sum(x_f[i,b] for i in followers, b in production_process) - sum(x_l[b] for b in production_process) == 0) #Markets clear
+for b in production_process
+	@constraint(m, K_l[b] - x_l[b] >= 0)
+end
+##
+for i in followers, b in production_process
+	@complements(m, 0 <= K_f[i,b] - x_f[i,b], gamma[i, b] >= 0)
+	@complements(m, 0 <= C_f[i, b] - (alpha - beta*q) + gamma[i, b], x_f[i, b] >= 0)
+end
+
+# print(m)
+solve(m)
+
+@show getobjectivevalue(m)
+Q = getvalue(q)
+X = getvalue(x_l)
+Price = alpha - beta*Q
+ConPmt = Price*Q
+ConSurp = alpha*Q - 0.5*beta*Q^2 - ConPmt
+
+Rev_l = Price*sum(X)
+VarCost_l = sum(C_l[b]*X[b] for b in production_process)
+ProdSurp = Rev_l - VarCost_l
+
+println("Price: ", Price, " leader production: ", X, ", consumer surplus: ", ConSurp, ", leader revenue: ", Rev_l, ", leader surplus: ", ProdSurp)
